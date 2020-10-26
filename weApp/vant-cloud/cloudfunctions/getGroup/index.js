971c7d360c77b78425577019e3ebb91b83527ecb
Angus <angus19990628@163.com>
2020-10-26 18:51:29 +0800

    小程序后端

--- weApp/vant-cloud/cloudfunctions/getGroup/index.js
+++ weApp/vant-cloud/cloudfunctions/getGroup/index.js
@@ -10,24 +10,27 @@ const db = cloud.database({ env }) //指明云函数生效的环境
 exports.main = async(event, context) => {
     const openId = cloud.getWXContext().openId
         // console.log(openId);
-    db.collection('user-group').where({
-        publishInfo: {
-            groupId: 'openId'
+    let groupList = await db.collection('user-group').where({ //groupList是{data:[]}
+        userId: openId
+    }).get()
+    let returnResult = []
+    for (let item of groupList.data) {
+        const oneGroup = await db.collection('group').where({
+                _id: item.groupId,
+                deleted: false
+            }).get()
+            // console.log(oneGroup);
+        if (oneGroup.data.length > 0) {
+            const userInfo = await db.collection('user').where({
+                openId: oneGroup.data[0].createBy
+            }).get()
+            oneGroup.data[0].createBy = userInfo.data[0]
+            oneGroup.data[0].relateUserGroupId = item._id
+            returnResult.push(oneGroup.data[0])
         }
-    }).get({
-        success: function(res) {
-            // 输出 [{ "title": "The Catcher in the Rye", ... }]
-            db.collection('group').where({
-                publishInfo: {
-                    _id: 'res.'
-                }
-            }).get({
-                success: function(res) {
-                    // 输出 [{ "title": "The Catcher in the Rye", ... }]
-                    // console.log(res)
-                }
-            })
-            console.log(res);
-        }
+    }
+    return returnResult.sort((a, b) => {
+        a.createTime < b.createTime ? 1 : -1
     })
+    return returnResult
 }
