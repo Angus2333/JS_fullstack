bd0464f32c56256bd3c0f4ba672efaa6ae1cd6a7
Angus <angus19990628@163.com>
2020-11-11 20:29:23 +0800

    进度条实现

--- ys_fullstack/js/闭包/1.js
+++ ys_fullstack/js/闭包/1.js
@@ -1,21 +1,23 @@
-function test() {
-  for(var i = 0; i < 10; i++) {
-
-    setTimeout((function(){
-      var temp=i;
-      return function(){
-        console.log(temp);
-      }
-       
-    })(),300)
-     
-    
-  }
-
-}
-
- test()
+// function test() {
+//     for (var i = 0; i < 10; i++) { // 用了var 会变量提升
+//         setTimeout((function() {
+//             var temp = i;
+//             return function() {
+//                 console.log(temp);
+//             }
+//         })(i), 300)
+//     }
+// }
+// test()
 
 // for(var j = 0; j < 10; j++) {
 //   myArr[j]()
 // }
+
+for (var i = 0; i < 10; i++) { // 用了var 会变量提升
+    (function(i) {
+        setTimeout(() => {
+            console.log(i);
+        }, i * 1000);
+    }(i));
+}
