8bac7bedcfc19dd4799b3bafd360ee9197b8e430
Angus <angus19990628@163.com>
2020-11-29 20:01:25 +0800

    vue

---
+++ vue/vue-attribute/src/views/Home.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="home">
+    <p>num: {{num}}</p>
+    <p>
+      <button @click="add">add</button>
+    </p>
+  </div>
+</template>
+
+<script>
+import { addLog, app } from './home.js'
+export default {
+  data () {
+    return {
+      num: 1
+    }
+  },
+  methods: {
+    add () {
+      this.num++
+      console.log(this.num ** 2)
+    }
+  },
+  created () {
+    console.log('我是原生的')
+  },
+  mixins: [addLog],
+  extends: app // 扩展
+}
+</script>
+
+<style>
+
+</style>
