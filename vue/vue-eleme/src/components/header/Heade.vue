e961d2f508c709e0214f44804f088cc87034e965
Angus <angus19990628@163.com>
2020-11-30 17:07:10 +0800

    vue

---
+++ vue/vue-eleme/src/components/header/Heade.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="header">
+    <div class="content-wrapper">
+      <div class="avatar">
+        <img
+          width="64px"
+          height="64px"
+          src="https://ss2.bdstatic.com/70cFvnSh_Q1YnxGkpoWK1HF6hhy/it/u=2944606985,3722907313&fm=11&gp=0.jpg"
+          class=""
+        />
+      </div>
+      <div class="content">
+        <div class="title">
+          <span class="brand"></span>
+          <span class="name">粥品香坊（回龙观）</span>
+        </div>
+        <div class="description">
+            蜂鸟专送/38分钟送达
+        </div>
+        <div class="support">
+          <span></span>
+          <span class="text">在线支付满28减5</span>
+        </div>
+      </div>
+    </div>
+    <div class="bulletin-wrapper"></div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
