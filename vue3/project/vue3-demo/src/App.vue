315d925eff273e7483c6b37fb912c23902e030b2
Angus <angus19990628@163.com>
2021-01-04 22:45:31 +0800

    vue3

---
+++ vue3/project/vue3-demo/src/App.vue
@@ -0,0 +1,51 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png">
+</template>
+
+<script>
+import { computed, onMounted, reactive,ref } from 'vue'
+
+export default {
+  name: 'App',
+  components: {
+  
+  },
+  setup(){ //composition api的入口
+    // const state=reactive({
+    //   count:1
+    // })
+    const {state,double}=useCounter(1)
+    let num = ref(1)
+    // const double=computed(()=>{
+    //   return state.count*2
+    // })
+    function add(){
+      state.count++;
+      num.value+=10
+    }
+    onMounted(()=>{
+
+    })
+    return {state,add,double,num}
+  }
+  
+}
+function useCounter(count){
+  const state=reactive({
+    count
+  })
+  const double=computed(()=>state.count*2)
+  return {state,double}
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ vue3/project/vue3-demo/src/components/Fargment.vue
@@ -0,0 +1,46 @@
+<template>
+  <quick :data="left" v-if="left.length"></quick>
+  <li>{{ flag }}</li>
+  <quick :data="right" v-if="right.length"></quick>
+</template>
+
+<script>
+export default {
+  name: "quick",
+  props: ['data'],
+  setup(props) {
+    let flag = props.data;
+    function quickSort(arr, i, j) {
+      if (i < j) {
+        let left = i;
+        let right = j;
+        let pivot = arr[left];
+        while (i < j) {
+          while (arr[j] >= pivot && i < j) {
+            // 从后往前找比基准小的数
+            j--;
+          }
+          if (i < j) {
+            arr[i++] = arr[j];
+          }
+          while (arr[i] <= pivot && i < j) {
+            // 从前往后找比基准大的数
+            i++;
+          }
+          if (i < j) {
+            arr[j--] = arr[i];
+          }
+        }
+        arr[i] = pivot;
+        quickSort(arr, left, i - 1);
+        quickSort(arr, i + 1, right);
+        return arr;
+      }
+    }
+    quickSort(flag,0,flag.length-1)
+  },
+};
+</script>
+
+<style>
+</style>
